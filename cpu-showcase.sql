1962929608de6c25746b44a2292eee9cf0ce4c25
Riccardo Barichella <95440224+baricio00@users.noreply.github.com>
2024-08-28 06:36:26 +0200

    Added the database backup

---
+++ cpu-showcase.sql
@@ -0,0 +1,40 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.4
+-- Dumped by pg_dump version 16.4
+
+-- Started on 2024-08-28 06:32:42
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 4836 (class 0 OID 16422)
+-- Dependencies: 216
+-- Data for Name: socket; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.socket VALUES ('LGA1200');
+INSERT INTO public.socket VALUES ('AM4');
+INSERT INTO public.socket VALUES ('TR4');
+INSERT INTO public.socket VALUES ('sTRX4');
+INSERT INTO public.socket VALUES ('LGA1700');
+INSERT INTO public.socket VALUES ('AM5');
+
+
+-- Completed on 2024-08-28 06:32:42
+
+--
+-- PostgreSQL database dump complete
+--
+
